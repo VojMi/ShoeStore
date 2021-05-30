1c223f61bdd60dfbc5505ba1c03ea72e8502f0cb
VojMi <vojtech.miklas@gmail.com>
2021-05-30 20:24:23 +0200

    Dependencies added, fragments and Navigation Graph created

---
+++ app/src/main/java/com/udacity/shoestore/ListFragment.kt
@@ -0,0 +1,31 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class ListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ListFragment()
+    }
+
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
